315e8d3970cd09b5ad51d80ad95717586afdbcdb
JenVest2020 <jenvest2019@gmail.com>
2020-10-02 22:19:16 -0500

    db-config file, schema table, and users.db3 created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 4051;
+const port = process.env.PORT || 4052;
 
 server.listen(port, () => {
     console.log(`\n***Listening on port ${port}***\n`);
