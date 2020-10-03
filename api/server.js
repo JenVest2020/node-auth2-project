afafe4263395c5791f248822d5b6a70d0b980233
JenVest2020 <jenvest2019@gmail.com>
2020-10-03 17:46:27 -0500

    token/session/authRouter/completed

--- api/server.js
+++ api/server.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const authRouter = require('../routers/authRouter.js');
-
 const session = require('express-session');
 const knexSessionStore = require('connect-session-knex')(session);
+
+const userRouter = require('../users/userRouter');
+const authRouter = require('../auth/authRouter');
+
 
 const server = express();
 
 const sessionConfig = {
-    name: 'jvcookie',
-    secret: 'SSSHHHH',
+    name: 'jvcookie2',
+    secret: 'Its a secret',
     cookie: {
         maxAge: 1000 * 60 * 60,
         secure: false,
-        httpOnly: true,
+        httpOnly: true
     },
     resave: false,
     saveUninitialized: false,
 
-    store: new knexSessionStore({
+    Store: new knexSessionStore({
         knex: require('../data/db-config'),
+        tablename: 'sessions',
         sidfieldname: 'sid',
         createtable: true,
         clearInterval: 1000 * 60 * 60
@@ -32,6 +35,7 @@ server.use(express.json());
 server.use(helmet());
 
 server.use(cors());
+server.use('/api/users', userRouter);
 server.use('/api/auth', authRouter);
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,1 +1,14 @@
 const router = require('express').Router();
+const db = require('./usersModel');
+
+router.get('/', (req, res) => {
+    db.getUsers()
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'error retrieving users' });
+        })
+})
+
+module.exports = router;
